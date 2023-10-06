eab5d4f7193d3768d99c47b7c6a93e9e2baeb2ad
Kuba Kuba <jakiz.jakub@seznam.cz>
2023-10-06 14:19:25 +0200

    Hamburger

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Hamburger Menu
+let navLinks = document.getElementById("navLinks")
+
+    const showMenu = () => {
+        navLinks.style.right = "0"
+    }
+    const hideMenu = () => {
+        navLinks.style.right = "-250px"
+    }
